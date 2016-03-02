8be33d2b869302e1c74d0dd40422b7ab08365e49
Steve Barnett <steve@naga.co.za>
2016-03-02 14:30:00 +0200

    Add missing users.js

---
+++ node-tutorial/users.js
@@ -0,0 +1,12 @@
+// a empty user list
+var users = [];
+
+//create 3 users in the users array
+//using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
+
+//Create 3 users : Jack, Callum and Neil - they all should have the name Bradshaw
+
+//now loop through the users array and write the following data to the console below each other for each user
+//you can use the forEach method
+
+//Hi, first_name last_name!
