cace8d5bdb54446435d31340999b885a19d6bd05
zimmat <zimkhitha@projectcodex.co>
2016-03-23 08:46:06 +0200

    Add

--- node-tutorial/read-folder.js
+++ node-tutorial/read-folder.js
@@ -3,11 +3,9 @@
   var data = [];
 
   var readFolder = fs.readdirSync('./files/week1');
-  // return (files);
+
   readFolder.forEach(function(folder){
-    // for (var i = 0; i < files.length;i++){
-    //return(folder);
-  //}
+
   });
   readFolder.forEach(function(file) {
     var lines = fs.readFileSync('./files/week1/' + file, 'utf8');
@@ -27,17 +25,10 @@
     return data;
   }
 
-// //
-// files.forEach(function(file) {
-//       var linesInFilesAsync = fs.readFile('./files/week1/', err, file).split('\n');
-//       data.push(linesInFiles);
-//       if (err) throw err;
-//       console.log(linesInFiles);
-//     }
-//   );
 
-// exports.linesInFilesAsync = function(folderName, callback){
-//   var linesInFiles = findLines(folderName);
-//     callback(null, linesInFile)
-//     console.log(linesInFile);
-// }
+
+exports.linesInFilesAsync = function(folderName, callback){
+  var data = findLines(folderName);
+    callback(null, data);
+    return data;
+}
