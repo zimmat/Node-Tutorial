f1c2212567d6ae03f68f10853b76d28bd259d098
zimmat <zimkhitha@projectcodex.co>
2016-03-24 17:15:27 +0200

    Add products

--- node-tutorial/products.js
+++ node-tutorial/products.js
@@ -1,27 +1,38 @@
-  var fs = require('fs');
+var fs = require('fs');
 
-  module.exports = function(path) {
+module.exports = function(path) {
+  var  products = {};
 
-      this.productNames = function(callback) {
-        var list = fs.readFileSync('./files/products.csv', 'utf8');
-        var items = list.split('\n');
-        var productList = [];
-        var productSold = {};
+  this.productNames = function(callback) {
+    var list = fs.readFileSync('./files/products.csv', 'utf8');
+    var items = list.split('\n');
+    var productList = [];
 
-
-        items.forEach(function(item) {
-          var productName = item.split(",")[1];
-          if (productList.indexOf(productName) === -1) {
-            productList.push(productName);
-          }
-
-        })
-
-        callback(null, productList);
+    items.forEach(function(item) {
+      var productName = item.split(",")[1];
+      if (productList.indexOf(productName) === -1) {
+        productList.push(productName);
       }
-    }
+    });
+
+    callback(null, productList);
+  };
+
 this.productsSold = function(callback) {
-  var list = fs.readFileSync('./files/products.csv', 'utf8');
-  var items = list.split('\n');
-  
-}
+    var list = fs.readFileSync('./files/products.csv', 'utf8');
+    var items = list.split('\n');
+    items.forEach(function(item) {
+
+        var productName = item.split(",")[1];
+        var productsQuantity = Number(item.split(",")[2]);
+
+      if  (products[productName] === undefined){
+        products[productName] = productsQuantity;
+      } else {
+              products[productName] += productsQuantity;
+      }
+
+    });
+     callback(null, products);
+   };
+};
